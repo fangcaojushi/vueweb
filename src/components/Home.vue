d86dfcf72d9aeeda21778fab137f5ce3503bb2a6
fangcaojushi <fangcaojushi@163.com>
2021-03-01 21:36:23 +0800

    'init'

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+	<el-container>
+	  <Header />
+	  <el-main>Main</el-main>
+	  <el-footer>Footer</el-footer>
+	</el-container>
+</template>
+
+<script>
+	import Header from '@/components/common/Header.vue'
+	export default{
+		name: "Home",
+		components:{Header}
+	}
+</script>
+
+<style scoped="scoped">
+	.el-footer {
+	    background-color: #B3C0D1;
+	    color: #333;
+	    text-align: center;
+	    line-height: 60px;
+	  }
+</style>
