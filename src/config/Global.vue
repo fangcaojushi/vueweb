0354d0b607a9d78f9c9b9ee4a52cf638c8e3acc0
fangcaojushi <fangcaojushi@163.com>
2021-03-04 10:26:46 +0800

    public variable

---
+++ src/config/Global.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+  /***********************************
+   * 统一认证服务器地址
+   ***********************************/
+  // const baseUrl = "http://192.168.1.37:8004"; // 测试环境
+  const baseUrl = "http://online-integration.co-meeting.cn:8197"; // 生产环境
+
+  export default
+  {
+    baseUrl
+  }
+</script>
+
+<style>
+</style>
